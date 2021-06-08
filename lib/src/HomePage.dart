4f1e3c21ab8a72ecbd6bb815afe4e6021a041baf
Carlos Raúl Oliveros Alpírez <44912302+alpi2111@users.noreply.github.com>
2021-06-08 16:07:52 -0500

    Cambios en el home
    Se modificó el home para agregar el botón al final de la lista

--- lib/src/HomePage.dart
+++ lib/src/HomePage.dart
@@ -3,7 +3,7 @@ import 'package:liquid_swipe/liquid_swipe.dart';
 import 'package:totonaco_aplication/src/pages/Containers.dart';
 
 class HomePage extends StatelessWidget {
-  
+  final _totalPages = liquidPages.length;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +14,19 @@ class HomePage extends StatelessWidget {
         enableSlideIcon: true,
         positionSlideIcon: 0.8,
         waveType: WaveType.circularReveal,
+        onPageChanged: (page) {
+            if (page == liquidPages.length - 1 && _totalPages == liquidPages.length) {
+               //AQUI HAY UN SETSTATE ASI QUE DEBES CAMBIAR TU CLASE DE
+               // STATELESS TO STATEFULL PARA QUE FUNCIONE
+               setState(() {
+               addButton(
+                 ElevatedButton(
+                    onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => Escucha())),
+                    child: Text("¡Ir a la siguiente página!"),
+                ));
+               });
+            }
+        }
       ),
     );
   }
